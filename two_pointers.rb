4c52d6f000ed8fe41a47c12fc8112e5273aec4e4
Corrie <corrieleech@gmail.com>
2022-04-18 09:05:26 -0500

    adds todays practice

--- two_pointers.rb
+++ two_pointers.rb
@@ -63,7 +63,60 @@
 #add one to the index
 #return largest product
 
-def method_name
-  
-end
+# def largest_product(numbers)
+#   largest_product = numbers[0] * numbers[1]
+#   outer_index = 0
+#   while outer_index < numbers.length
+#     inner_index = outer_index + 1
+#     while inner_index < numbers.length - 1
+#       product = numbers[outer_index] * numbers[inner_index]
+#       if product > largest_product
+#         largest_product = product
+#       end
+#       inner_index += 1
+#     end
+#     outer_index += 1
+#   end
+#   largest_product
+# end
 
+# p largest_product([5, -2, 1, -9, -7, 2, 6])
+
+##Two Sum 1
+
+# Given an array of numbers, return a new array containing just two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false.
+
+# Specifically use nested loops to solve this exercise even though there are other approaches as well.
+
+# Input: [2, 5, 3, 1, 0, 7, 11]
+# Output: [3, 7]
+
+# Input: [1, 2, 3, 4, 5]
+# Output: false (While 1, 2, 3, and 4 altogether add up to 10, we're seeking just one pair of numbers.)
+
+#create a function that takes in array
+#create output array
+#using nested loop...
+#loop through each number in the array and check if it and the next number (inner loop) adds to 10. if so, return that array
+#if get to end and array.length = 0, return false
+
+# def two_sum(numbers)
+#   ten_pair = []
+#   outer_index = 0
+#   while outer_index < numbers.length
+#     inner_index = outer_index + 1
+#     while inner_index < numbers.length - 1
+#       sum = numbers[outer_index] + numbers[inner_index]
+#       if sum == 10
+#         ten_pair << numbers[outer_index]
+#         ten_pair << numbers[inner_index]
+#         return ten_pair
+#       end
+#       inner_index += 1
+#     end
+#     outer_index += 1
+#   end
+#   return false
+# end
+
+# p two_sum([2, 5, 3, 1, 0, 7, 11])
