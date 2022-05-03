db823af38423e22b41a4a085605120e5d3fd57a9
Corrie <corrieleech@gmail.com>
2022-05-02 21:12:55 -0500

    adds today's homework

---
+++ codesignal.js
@@ -0,0 +1,23 @@
+// function solution(statues) {
+//   var completeSet = statues.sort(function (numberA, numberB) {
+//     return numberA - numberB;
+//   });
+//   console.log(completeSet);
+//   var index = 0;
+//   var missingStatues = 0;
+//   while (index < completeSet.length - 1) {
+//     if (completeSet[index + 1] !== completeSet[index] + 1) {
+//       var numberMissing = completeSet[index + 1] - completeSet[index] - 1;
+//       missingStatues = missingStatues + numberMissing;
+//       console.log(missingStatues);
+//     }
+//     index += 1;
+//   }
+//   return missingStatues;
+// }
+
+// solution([6, 2, 3, 8]);
+
+// function solution(sequence) {
+//   return Math.max(...sequence)-Math.min(...sequence)+1-sequence.length
+// }
